19a2fb5756973610576160cdf29254817d3f0f87
linhx <1151676384@qq.com>
2021-05-28 17:59:08 +0800

    vue3.0使用完成

---
+++ src/8.ref操作demo的使用.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1 ref="hRef">哈哈</h1>
+  </div>
+</template>
+
+<script>
+import { onMounted, ref } from "vue";
+// ref的使用
+export default {
+  setup() {
+    let hRef = ref(null);
+    onMounted(() => {
+      console.log(11, hRef.value);
+    });
+    return {
+      hRef,
+    };
+  },
+};
+</script>
+
+
